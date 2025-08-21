51e678b24d7bb0f17111c940b1779f6f3a2bff88
avelasquezz <juanandresv2004@gmail.com>
2025-08-21 17:32:11 -0500

    fix: add an 's' in the test file path

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from bminor.lexer import Lexer
 from bminor.errors import errors_detected
 
 if __name__ == "__main__":
-  with open("./test/scanner/sieve.bminor", "r") as file:
+  with open("./tests/scanner/sieve.bminor", "r") as file:
     code = file.read()
   
   lexer = Lexer()
