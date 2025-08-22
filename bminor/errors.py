7a4b50b8738ecc87ebdfe27f975759a0e6e0e0da
avelasquezz <juanandresv2004@gmail.com>
2025-08-21 21:28:27 -0500

    refactor: use ternary operator instead if-else structure

--- bminor/errors.py
+++ bminor/errors.py
@@ -6,18 +6,20 @@ _errors_detected = 0
 
 def error(message, lineno=None):
   global _errors_detected
-  if lineno:
-    print(f"{lineno}: [red]{message}[/red]")
-  else:
-    print(f"[red]{message}[/red]")
   _errors_detected += 1
+
+  where = f"{lineno}: " if lineno else ""
+
+  print(f"[red]{where}{message}[/red]")
  
 def errors_detected():
   global _errors_detected
-  if _errors_detected == 0:
-    print(f"[green]Errors detected: {_errors_detected}[/green]")
-  else:
-    print(f"[red]Errors detected: {_errors_detected}[/red]")
+
+  print(
+      f"[red]Errors: {_errors_detected}[/red]" if _errors_detected 
+    else 
+      f"[green]Errors: {_errors_detected}[/green]"
+  )
 
 def clear_errors():
   global _errors_detected
