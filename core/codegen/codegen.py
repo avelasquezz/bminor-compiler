8140c6411ce774efbd7c029d9e34b8e97c1cd74f
avelasquezz <juanandresv2004@gmail.com>
2025-10-31 22:42:07 -0500

    feat: add LLVM IR generation for variables, literals, and functions

--- core/codegen/codegen.py
+++ core/codegen/codegen.py
@@ -1,69 +1,120 @@
-from core.parser.model import Visitor
+from core.parser.model import *
 from llvmlite          import ir
+
+# LLVM types corresponding to the B-Minor types
+int_type   = ir.IntType(32)
+float_type = ir.DoubleType()
+bool_type  = ir.IntType(1)
+char_type  = ir.IntType(8)
+void_type  = ir.VoidType()
+
+_typemap = {
+	"integer": int_type,
+	"float"  : float_type,
+	"boolean": bool_type,
+	"char"   : char_type,
+  "void"   : void_type
+}
 
 class CodeGenerator(Visitor):
   def __init__(self):
     self.module = ir.Module(name="bminor_module")
     self.builder = None
     self.symbols = {}
+  
+  def visit(self, n: Node):
+    print(f"Node '{n.__class__.__name__}' not implemented")
 
-  def visit(self, node, *args, **kwargs):
-    method_name = 'visit_' + node.__class__.__name__
-    visitor = getattr(self, method_name, self.generic_visit)
-    return visitor(node, *args, **kwargs)
-
-  def generic_visit(self, node, *args, **kwargs):
-    raise NotImplementedError(f"No visit_{node.__class__.__name__} method")
-
-  def visit_Program(self, node):
-    main_ty = ir.FunctionType(ir.IntType(32), [])
-    main_fn = ir.Function(self.module, main_ty, name="main")
-    block = main_fn.append_basic_block(name="entry")
-    self.builder = ir.IRBuilder(block)
-
+  def visit(self, node: Program):
     for decl in node.body:
       decl.accept(self)
 
-    self.builder.ret(ir.Constant(ir.IntType(32), 0))
+  def visit(self, node: VarDecl):
+    ty = _typemap[node.type]
 
-  def visit_VarDecl(self, node):
-    int32 = ir.IntType(32)
-    ptr = self.builder.alloca(int32, name=node.name)
+    if self.builder is None:
+      global_var = ir.GlobalVariable(self.module, ty, name=node.name)
+      global_var.linkage = 'common'
+      global_var.initializer = node.value.accept(self) if node.value else ir.Constant(ty, None)
+      self.symbols[node.name] = global_var
+      return
+
+    ptr = self.builder.alloca(ty, name=node.name)
     self.symbols[node.name] = ptr
 
     if node.value:
       val = node.value.accept(self)
       self.builder.store(val, ptr)
 
-  def visit_Literal(self, node):
-    if node.type == "integer":
-      return ir.Constant(ir.IntType(32), int(node.value))
-    else:
-      raise NotImplementedError(f"Literal type {node.type} not supported")
+  def visit(self, node: Literal):
+    match node.type:
+      case "integer":
+        return ir.Constant(int_type, int(node.value))
+      case "float":
+        return ir.Constant(float_type, float(node.value))
+      case "boolean":
+        return ir.Constant(bool_type, 1 if node.value == "true" else 0)
+      case "char":
+        return ir.Constant(char_type, ord(node.value))
         
-  def visit_VarLoc(self, node):
-    """
-    Genera el código para acceder al valor de una variable.
-    - node.name: nombre de la variable
-    """
-    if node.name not in self.symbols:
-      raise NameError(f"Variable '{node.name}' not declared")
-
+  def visit(self, node: VarLoc):
     ptr = self.symbols[node.name]
-    # Cargamos el valor almacenado en memoria
     return self.builder.load(ptr, name=node.name)
 
-  def visit_BinOper(self, node):
+  def visit(self, node: BinOper):
     left = node.left.accept(self)
     right = node.right.accept(self)
 
-    if node.oper == '+':
-      return self.builder.add(left, right)
-    elif node.oper == '-':
-      return self.builder.sub(left, right)
-    elif node.oper == '*':
-      return self.builder.mul(left, right)
-    elif node.oper == '/':
-      return self.builder.sdiv(left, right)
-    else:
-      raise NotImplementedError(f"Operator {node.oper} not supported")
+    match node.oper:
+      case "+":
+        return self.builder.add(left, right)
+      case "-":
+        return self.builder.sub(left, right)
+      case "*":
+        return self.builder.mul(left, right)
+      case "/":
+        return self.builder.sdiv(left, right)
+  
+  def visit(self, node: FuncDecl):
+    param_types = [_typemap[p.type] for p in node.params]
+
+    ty = _typemap[node.type]
+    func_ty = ir.FunctionType(ty, param_types)
+    func = ir.Function(self.module, func_ty, name=node.name)
+    block = func.append_basic_block(name="entry")
+    self.builder = ir.IRBuilder(block)
+  
+    for arg, param in zip(func.args, node.params):
+      arg.name = param.name
+      ptr = self.builder.alloca(arg.type, name=param.name)
+      self.builder.store(arg, ptr)
+      self.symbols[param.name] = ptr
+
+    for decl in node.body:
+      decl.accept(self)
+
+    if not self.builder.block.is_terminated:
+      if ty == void_type:
+        self.builder.ret_void()
+      else:
+        self.builder.ret(ir.Constant(ty, 0))
+  
+  def visit(self, node: ReturnStmt):
+    retval = node.value.accept(self)
+    self.builder.ret(retval)
+
+if __name__ == '__main__':
+  import sys
+
+  from core.parser.parser import parse
+
+  if len(sys.argv) != 2:
+	  raise SystemExit("Usage: python dotrender.py <filename>")
+
+  txt = open(sys.argv[1], encoding='utf-8').read()
+  ast = parse(txt)
+  
+  cg = CodeGenerator()
+  cg.visit(ast)
+
+  print(cg.module)
